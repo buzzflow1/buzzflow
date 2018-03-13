731fb791312333c9abd69c6a300a326de3d4683c
西森大介 <nishimori.daisuke@di-v.co.jp>
2018-03-13 22:04:21 +0900

    config automatically generation

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,14 @@ module Buzzflow
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      # g.template_engine false 
+      # g.test_framework :rspec, view_specs: false, helper_specs: false, fixture: true
+      # g.fixture_replacement :factory_girl, dir: "spec/support/factories"
+    end
   end
 end
